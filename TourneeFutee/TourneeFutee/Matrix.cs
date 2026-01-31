49e0ab7d3b826388fa146c6429a4df01b0703215
Ludovic ANDRÉ <ludovic.andre@edu.devinci.fr>
2026-01-31 12:06:25 +0100

    Matrix a finir

--- TourneeFutee/TourneeFutee/Matrix.cs
+++ TourneeFutee/TourneeFutee/Matrix.cs
@@ -71,31 +71,28 @@
         {
             if (i < 0 || i > nbRows)
             {
-                throw new ArgumentException();
+                throw new ArgumentOutOfRangeException();
             }
-            else 
+            else
             {
-                listeS.Add(new List<float>(nbColums));
-                for (int j = i; j < nbRows + 1; j++)
+                float[][] newtabS = new float[nbRows + 1][];
+                for (int j = 0; j < i; j++)
                 {
-                    List<float> liste = listeS[j];
-                    listeS[j] = listeS[i]
-
-
+                    newtabS[j] = tabS[j];
                 }
-                    for (int j = i; j < nbRows + 1; j++) 
+                newtabS[i] = new float[nbColumns];
+                for (int k = 0; k < nbColumns; k++)
                 {
-                    for (int k = 0; k < nbColumns; k++) 
-                    {
-                        if (j == i) 
-                        {
-                            listeS.Add(new List<float>());
-                        }
-                    }
+                    newtabS[i][k] = defaultValue;
                 }
-            }
+                for (int j = i; j < nbRows; j++)
+                {
+                    newtabS[j + 1] = tabS[j];
+                }
+                tabS = newtabS;
+                nbRows++;
+            }   
         }
-
         /* Insère une colonne à l'indice `j`. Décale les colonnes suivantes vers la droite.
          * Toutes les cases de la nouvelle ligne contiennent DefaultValue.
          * Si `j` = NbColums, insère une colonne en fin de matrice
@@ -103,14 +100,61 @@
          */
         public void AddColumn(int j)
         {
-            // TODO : implémenter
+            if (i < 0 || i > nbColumn)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                float[][] newtabS = new float[nbRows][];
+                for (int k = 0; k < nbRows; k++)
+                {
+                    for(int l=0; l<j; l++)
+                    {
+                        newtabS[k][l] = tabS[k][l];
+                    }
+                    newtabS[j]=new
+                }
+                for (int k = 0; k < j)
+                {
+                    newtabS[k][l] = tabS[k][l];
+                }
+                newtabS[i] = new float[nbColumns];
+                for (int k = 0; k < nbColumns; k++)
+                {
+                    newtabS[i][k] = defaultValue;
+                }
+                for (int j = i; j < nbRows; j++)
+                {
+                    newtabS[j + 1] = tabS[j];
+                }
+                tabS = newtabS;
+                nbRows++;
+            }
+        }
         }
 
         // Supprime la ligne à l'indice `i`. Décale les lignes suivantes vers le haut.
         // Lève une ArgumentOutOfRangeException si `i` est en dehors des indices valides
         public void RemoveRow(int i)
         {
-            // TODO : implémenter
+            if (i < 0 || i >= nbRows)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            float[][] newtabS = new float[nbRows - 1][];
+            for (int j = 0; j < i; j++)
+            {
+                newtabS[j] = tabS[j];
+            }
+            for (int j = i + 1; j < nbRows; j++)
+            {
+                newtabS[j - 1] = tabS[j];
+            }
+
+            // 4. Mise à jour
+            tabS = newtabS;
+            nbRows--;
         }
 
         // Supprime la colonne à l'indice `j`. Décale les colonnes suivantes vers la gauche.
