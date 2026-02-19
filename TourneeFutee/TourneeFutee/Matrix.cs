84cb2bf2be414e087d718fa66ff7a03df920cf56
Bouaiche Chaima <chaima.bouaiche@ensia.edu.dz>
2026-02-19 19:05:36 +0100

    Fonctions de Matrix.cs effectué + 20 tests de matrix

--- TourneeFutee/TourneeFutee/Matrix.cs
+++ TourneeFutee/TourneeFutee/Matrix.cs
@@ -18,20 +18,20 @@
          */
         public Matrix(int nbRows = 0, int nbColumns = 0, float defaultValue = 0)
         {
-            if (nbRows < 0 || nbColumns < 0)
+
+            if (nbRows < 0) throw new ArgumentOutOfRangeException(nameof(nbRows));
+            if (nbColumns < 0) throw new ArgumentOutOfRangeException(nameof(nbColumns));
+
+            this.nbRows = nbRows;
+            this.nbColumns = nbColumns;
+            this.defaultValue = defaultValue;
+
+
+            tabS = new float[nbRows][];
+            for (int i = 0; i < nbRows; i++)
             {
-                throw new ArgumentException();
-            }
-            else
-            {
-                this.nbRows = nbRows;
-                this.nbColumns = nbColumns;
-                this.defaultValue = defaultValue;
-            }
-            this.tabS = new tab[nbRows][];
-            for (int i = 0; i < nbRows; i++) 
-            {
-                for (int j = 0; j < nbColumns; j++) 
+                tabS[i] = new float[nbColumns];
+                for (int j = 0; j < nbColumns; j++)
                 {
                     tabS[i][j] = defaultValue;
                 }
@@ -42,26 +42,29 @@
         // Lecture seule
         public float DefaultValue
         {
-            get; // TODO : implémenter
-                 // pas de set
+            get { return defaultValue; }
+            // pas de set
         }
 
         // Propriété : nombre de lignes
         // Lecture seule
         public int NbRows
         {
-            get; // TODO : implémenter
-                 // pas de set
+            get { return nbRows; } // TODO : implémenter 
+                                   // pas de set
         }
 
         // Propriété : nombre de colonnes
         // Lecture seule
         public int NbColumns
         {
-            get; // TODO : implémenter
-                 // pas de set
+            get { return nbColumns; } // TODO : implémenter
+                                      // pas de set
         }
-        public float[][] tabS { get; set; }
+        public float[][] TabS
+        {
+            get { return tabS; }
+        }
 
         /* Insère une ligne à l'indice `i`. Décale les lignes suivantes vers le bas.
          * Toutes les cases de la nouvelle ligne contiennent DefaultValue.
@@ -71,34 +74,40 @@
         public void AddRow(int i)
         {
             if (i < 0 || i > nbRows)
+                throw new ArgumentOutOfRangeException(nameof(i));
+
+            //  nouvelle structure avec +1 ligne
+            float[][] newtabS = new float[nbRows + 1][];
+
+            // copier les lignes avant i
+            for (int r = 0; r < i; r++)
             {
-                throw new ArgumentOutOfRangeException();
-            }
-            else
-            {
-                float[][] newtabS = new float[nbRows + 1][];
-                for (int j = 0; j < i; j++)
-                {
-                    newtabS[j] = tabS[j];
-                }
-                newtabS[i] = new float[nbColumns];
-                for (int k = 0; k < nbColumns; k++)
-                {
-                    newtabS[i][k] = defaultValue;
-                }
-<<<<<<< Updated upstream
-                for (int j = i; j < nbRows; j++)
-                {
-                    newtabS[j + 1] = tabS[j];
-                }
-                tabS = newtabS;
-                nbRows++;
-            }   
-=======
+                newtabS[r] = new float[nbColumns];
+                for (int c = 0; c < nbColumns; c++)
+                    newtabS[r][c] = tabS[r][c];
             }
 
->>>>>>> Stashed changes
+            // insérer la nouvelle ligne i remplie avec defaultValue
+            newtabS[i] = new float[nbColumns];
+            for (int c = 0; c < nbColumns; c++)
+                newtabS[i][c] = defaultValue;
+
+            //  copier les lignes à partir de i (décalées de +1)
+            for (int r = i; r < nbRows; r++)
+            {
+                newtabS[r + 1] = new float[nbColumns];
+                for (int c = 0; c < nbColumns; c++)
+                    newtabS[r + 1][c] = tabS[r][c];
+            }
+
+
+            tabS = newtabS;
+            nbRows++;
         }
+
+
+
+
         /* Insère une colonne à l'indice `j`. Décale les colonnes suivantes vers la droite.
          * Toutes les cases de la nouvelle ligne contiennent DefaultValue.
          * Si `j` = NbColums, insère une colonne en fin de matrice
@@ -106,95 +115,135 @@
          */
         public void AddColumn(int j)
         {
-            if (i < 0 || i > nbColumn)
+            if (j < 0 || j > nbColumns)
+                throw new ArgumentOutOfRangeException(nameof(j));
+
+            float[][] newtabS = new float[nbRows][];
+
+            for (int r = 0; r < nbRows; r++)
             {
-                throw new ArgumentOutOfRangeException();
+                newtabS[r] = new float[nbColumns + 1];
+
+                // Copier les colonnes avant j
+                for (int c = 0; c < j; c++)
+                    newtabS[r][c] = tabS[r][c];
+
+                // Nouvelle colonne j remplie avec defaultValue
+                newtabS[r][j] = defaultValue;
+
+                // Copier les colonnes à partir de j (décalées de +1)
+                for (int c = j; c < nbColumns; c++)
+                    newtabS[r][c + 1] = tabS[r][c];
             }
-            else
-            {
-                float[][] newtabS = new float[nbRows][];
-                for (int k = 0; k < nbRows; k++)
-                {
-                    for(int l=0; l<j; l++)
-                    {
-                        newtabS[k][l] = tabS[k][l];
-                    }
-                    newtabS[j]=new
-                }
-                for (int k = 0; k < j)
-                {
-                    newtabS[k][l] = tabS[k][l];
-                }
-                newtabS[i] = new float[nbColumns];
-                for (int k = 0; k < nbColumns; k++)
-                {
-                    newtabS[i][k] = defaultValue;
-                }
-                for (int j = i; j < nbRows; j++)
-                {
-                    newtabS[j + 1] = tabS[j];
-                }
-                tabS = newtabS;
-                nbRows++;
-            }
+
+            tabS = newtabS;
+            nbColumns++;
         }
-        }
+
+
 
         // Supprime la ligne à l'indice `i`. Décale les lignes suivantes vers le haut.
         // Lève une ArgumentOutOfRangeException si `i` est en dehors des indices valides
         public void RemoveRow(int i)
         {
             if (i < 0 || i >= nbRows)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
+                throw new ArgumentOutOfRangeException(nameof(i));
+
             float[][] newtabS = new float[nbRows - 1][];
-            for (int j = 0; j < i; j++)
+
+            // Lignes avant i
+            for (int r = 0; r < i; r++)
             {
-                newtabS[j] = tabS[j];
-            }
-            for (int j = i + 1; j < nbRows; j++)
-            {
-                newtabS[j - 1] = tabS[j];
+                newtabS[r] = new float[nbColumns];
+                for (int c = 0; c < nbColumns; c++)
+                    newtabS[r][c] = tabS[r][c];
             }
 
-            // 4. Mise à jour
+            // Lignes après i (décalées de -1)
+            for (int r = i + 1; r < nbRows; r++)
+            {
+                newtabS[r - 1] = new float[nbColumns];
+                for (int c = 0; c < nbColumns; c++)
+                    newtabS[r - 1][c] = tabS[r][c];
+            }
+
             tabS = newtabS;
             nbRows--;
         }
+
 
         // Supprime la colonne à l'indice `j`. Décale les colonnes suivantes vers la gauche.
         // Lève une ArgumentOutOfRangeException si `j` est en dehors des indices valides
         public void RemoveColumn(int j)
         {
-            // TODO : implémenter
+            if (j < 0 || j >= nbColumns)
+                throw new ArgumentOutOfRangeException(nameof(j));
+
+            float[][] newtabS = new float[nbRows][];
+
+            for (int r = 0; r < nbRows; r++)
+            {
+                newtabS[r] = new float[nbColumns - 1];
+
+                // Colonnes avant j
+                for (int c = 0; c < j; c++)
+                    newtabS[r][c] = tabS[r][c];
+
+                // Colonnes après j (décalées de -1)
+                for (int c = j + 1; c < nbColumns; c++)
+                    newtabS[r][c - 1] = tabS[r][c];
+            }
+
+            tabS = newtabS;
+            nbColumns--;
         }
+
 
         // Renvoie la valeur à la ligne `i` et colonne `j`
         // Lève une ArgumentOutOfRangeException si `i` ou `j` est en dehors des indices valides
         public float GetValue(int i, int j)
         {
-            // TODO : implémenter
-            return 0.0f;
+            if (i < 0 || i >= nbRows)
+                throw new ArgumentOutOfRangeException(nameof(i));
+            if (j < 0 || j >= nbColumns)
+                throw new ArgumentOutOfRangeException(nameof(j));
+
+            return tabS[i][j];
         }
+
 
         // Affecte la valeur à la ligne `i` et colonne `j` à `v`
         // Lève une ArgumentOutOfRangeException si `i` ou `j` est en dehors des indices valides
         public void SetValue(int i, int j, float v)
         {
-            // TODO : implémenter
+            if (i < 0 || i >= nbRows)
+                throw new ArgumentOutOfRangeException(nameof(i));
+            if (j < 0 || j >= nbColumns)
+                throw new ArgumentOutOfRangeException(nameof(j));
+
+            tabS[i][j] = v;
         }
+
 
         // Affiche la matrice
         public void Print()
         {
-            // TODO : implémenter
+            for (int r = 0; r < nbRows; r++)
+            {
+                for (int c = 0; c < nbColumns; c++)
+                {
+                    Console.Write(tabS[r][c]);
+                    if (c < nbColumns - 1) Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
         }
+
 
 
         // TODO : ajouter toutes les méthodes que vous jugerez pertinentes 
 
     }
-
-
 }
+
+
