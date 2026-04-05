5659839fd3a1de24e58c66ee5915439904aa181a
Elise202 <elise.bernard204@gmail.com>
2026-04-05 18:56:20 +0200

    implémentation des méthodes ReduceMatrix, GetMaxRegret (avec les méthodes GetMinInRowExcluding GetMinInolumnExcluding) et IsForbiddenSegment

--- TourneeFutee/TourneeFutee/Little.cs
+++ TourneeFutee/TourneeFutee/Little.cs
@@ -5,11 +5,16 @@
     public class Little
     {
         // TODO : ajouter tous les attributs que vous jugerez pertinents 
+        private Graph graph;
+        private int nbCities;
 
         // Instancie le planificateur en spécifiant le graphe modélisant un problème de voyageur de commerce
         public Little(Graph graph)
         {
             // TODO : implémenter
+            this.graph = graph;
+            this.nbCities = graph.Order;
+
         }
 
         // Trouve la tournée optimale dans le graphe `this.graph`
@@ -28,7 +33,73 @@
         public static float ReduceMatrix(Matrix m)
         {
             // TODO : implémenter
-            return 0.0f;
+
+            float TotalReduction = 0;
+
+            // Réduction des lignes 
+            for(int i=0;i<m.NbRows;i++)
+            {
+                float minRow = float.PositiveInfinity;
+
+                // On cherche le minimum de la ligne
+                for (int j=0;j<m.NbColumns;j++)
+                {
+                    float val = m.GetValue(i, j);
+                    if(val<minRow)
+                    {
+                        minRow = val;   
+
+                    }
+                }
+
+                // Si on trouve un minimum non nulle et différent de l'infini, on effectue le code suivant
+                if(minRow>0 && minRow!= float.PositiveInfinity)
+                {
+                    for(int j = 0;j<m.NbColumns; j++)
+                    {
+                        float val = m.GetValue(i, j);
+                        if(val!=float.PositiveInfinity)
+                        {
+                            m.SetValue(i, j, val-minRow);
+                        }
+                    }
+                    TotalReduction += minRow;
+                }
+
+                
+
+            }
+            // Réduction des colonnes
+            for (int j = 0; j < m.NbColumns; j++)
+            {
+                float minCol = float.PositiveInfinity;
+
+                //On cherche le minimum de la colonne
+                for (int i = 0; i < m.NbRows; i++)
+                {
+                    float val = (m.GetValue(i, j));
+                    if(val<minCol)
+                    {
+                        minCol= val;
+                    }
+
+                }
+                // Si on trouve un minimum non nulle et différent de l'infini, on effectue le code suivant
+                if (minCol>0 && minCol!= float.PositiveInfinity)
+                {
+                    for(int i = 0; i < m.NbRows; i++)
+                    {
+                        float val = m.GetValue(i, j);
+                        if(val!=float.PositiveInfinity)
+                        {
+                            m.SetValue(i, j, val-minCol);
+                        }
+                        
+                    }
+                    TotalReduction += minCol;
+                }
+            }
+            return  TotalReduction;
         }
 
         // Renvoie le regret de valeur maximale dans la matrice de coûts `m` sous la forme d'un tuple `(int i, int j, float value)`
@@ -36,7 +107,33 @@
         public static (int i, int j, float value) GetMaxRegret(Matrix m)
         {
             // TODO : implémenter
-            return (0, 0, 0.0f);
+            int bestI = -1;
+            int bestJ = -1;
+            float maxRegret = -1.0f;
+
+            for(int i=0;i<m.NbRows;i++)
+            {
+                for(int j=0;j<m.NbColumns;j++)
+                {
+                    if(m.GetValue(i, j)==0)
+                    {
+                        float minRow = GetMinInRowExcluding(m, i, j);
+
+                        float minCol = GetMinInColumnExcluding(m, j, i);
+
+                        float currentRegret = minCol + minRow;
+
+                        if(currentRegret>maxRegret)
+                        {
+                            maxRegret = currentRegret;
+                            bestI = i;
+                            bestJ= j;
+
+                        }
+                    }
+                }
+            }
+            return (bestI, bestJ, maxRegret);
 
         }
 
@@ -47,10 +144,70 @@
         {
 
             // TODO : implémenter
+            string currentCity = segment.destination;
+            bool foundNext = true;
+
+            while(foundNext)
+            {
+                foundNext = false;
+
+                // on cherche si un segment déjà inclus part de la ville actuelle
+                foreach(var s in includedSegments)
+                {
+                    if(s.source == currentCity)
+                    {
+                        currentCity = s.destination;
+                        foundNext = true;
+                        break;
+                    }
+                    
+                }
+            }
+            if(currentCity==segment.source)
+            {
+                return (includedSegments.Count + 1 < nbCities);
+            }
             return false;   
         }
 
         // TODO : ajouter toutes les méthodes que vous jugerez pertinentes 
 
+        private static float GetMinInRowExcluding(Matrix m,int row, int coltoexclude)
+        {
+            float min = float.PositiveInfinity;
+            for(int j=0;j<m.NbColumns;j++)
+            {
+                if(j==coltoexclude)
+                {
+                    continue;
+                }
+                float val = m.GetValue(row,j);
+                if(val<min)
+                {
+                    min= val;
+                }
+            }
+
+            return min;
+        }
+
+        private static float GetMinInColumnExcluding(Matrix m, int col, int rowToExclude)
+        {
+            float min = float.PositiveInfinity;
+            for(int i=0;i<m.NbRows;i++)
+            {
+                if(i==rowToExclude)
+                {
+                    continue;
+                }
+                float val= m.GetValue(i,col);
+                if(val<min)
+                {
+                    min= val;
+                }
+
+            }
+            return min;
+        }
     }
 }
